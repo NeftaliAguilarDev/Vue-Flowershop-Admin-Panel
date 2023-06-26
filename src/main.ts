6d1f295daa29eece0b64108d05e31648e0dce8eb
Neftali-Aguilar <iaguilar.neftali@gmail.com>
2023-06-26 02:48:26 -0600

    Feat: Get customer data from GraphQl

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-// const vuetify = createVuetify();
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-createApp(App).use(router).use(vuetify).mount('#app')
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://api-flowershop.test/graphql'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App)
+})
+
+app.use(router).use(vuetify).mount('#app')
--- src/views/customers/Index.vue
+++ src/views/customers/Index.vue
@@ -5,9 +5,12 @@
                 <v-card>
                     <div class="flex justify-between mt-5 items-center">
                         <v-card-title class="text-purple-500 !font-extrabold">Customer</v-card-title>
-                        <router-link :to="{ name: 'customers.create' }" class="mr-5 px-5 py-2 bg-purple-500 hover:bg-purple-600 rounded-md text-white">Create customer</router-link>
+                        <router-link :to="{ name: 'customers.create' }"
+                            class="mr-5 px-5 py-2 bg-purple-500 hover:bg-purple-600 rounded-md text-white">Create
+                            customer</router-link>
                     </div>
-                    <v-data-table :headers="headers" :items="customers" class="elevation-1">
+
+                    <v-data-table :loading="loadingData" :headers="headers" :items="customers" class="elevation-1">
                     </v-data-table>
                 </v-card>
             </v-col>
@@ -16,9 +19,30 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag';
+import { reactive, ref } from 'vue';
 import { VDataTable } from 'vuetify/labs/VDataTable'
-
+const loadingData = ref(true)
+const { result } = useQuery(gql`
+      query getCustomers {
+        customers {
+          id,
+          name,
+          phone,
+          email
+        }
+      }
+    `)
+const customers = reactive([])
+setTimeout(() => {
+    const { customers: items } = result.value
+    console.log(items)
+    items.forEach(item => {
+        customers.push(item)
+    });
+    loadingData.value = false
+}, 1000);
 const headers = reactive([
     {
         title: '# ID',
@@ -30,23 +54,6 @@ const headers = reactive([
     { title: 'Phone', key: 'phone' },
     { title: 'Email', key: 'email' },
 ]);
-
-const customers = reactive([
-    {
-        id: 1,
-        name: 'Customer name',
-        phone: 12321321,
-        email: 'customer@customer.com',
-    },
-    {
-        id: 2,
-        name: 'Customer 2',
-        phone: 12313123,
-        email: 'customer@customer.com',
-    },
-
-])
-
 </script>
 
 <style scoped></style>
